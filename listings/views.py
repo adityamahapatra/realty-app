7eb3ad3009dd4ff962f7fc678e31c077f0dc1350
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-12 05:13:52 +0530

    Fully set up the listings app.

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,15 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request) -> HttpResponse:
+    return render(request, "listings/listings.html")
+
+
+def listing(request) -> HttpResponse:
+    return render(request, "listings/listing.html")
+
+
+def search(request) -> HttpResponse:
+    return render(request, "listings/search.html")
--- realty/settings.py
+++ realty/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "listings.apps.ListingsConfig",
     "pages.apps.PagesConfig",
     "django.contrib.admin",
     "django.contrib.auth",
