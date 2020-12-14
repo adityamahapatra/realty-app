8ef88d433c63fb61017e39b2b1a534fbafaab13c
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-14 05:42:12 +0530

    Customize the way the admin panel displays the data.

--- realtors/admin.py
+++ realtors/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
+from django.db import models
 
 # Register your models here.
 from .models import Realtor
 
-admin.site.register(Realtor)
+
+class RealtorAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "email", "hire_date")
+    list_display_links = ("id", "name")
+    search_fields = ("name",)
+    list_per_page = 25
+
+
+admin.site.register(Realtor, RealtorAdmin)
