17ed84e83eefc9d4ea265f15b1e8da4f24da17e5
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-25 04:57:26 +0530

    Make the home page content dynamic.

--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from listings.models import Listing
+
 
 # Create your views here.
 def index(request) -> HttpResponse:
-    return render(request, "pages/index.html")
+    listings = Listing.objects.order_by("-list_date")
+    listings = listings.filter(is_published=True)[:3]
+
+    context = {
+        "listings": listings,
+    }
+
+    return render(request, "pages/index.html", context)
 
 
 def about(request) -> HttpResponse:
