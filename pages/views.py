92ce5258f9afe47633f472fa96dc83bf5ec33b5b
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-26 03:28:48 +0530

    Make the search form options content dynamic.

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 from listings.models import Listing
 from realtors.models import Realtor
+from listings.choices import bedroom_choices, price_choices, state_choices
 
 
 # Create your views here.
@@ -12,6 +13,9 @@ def index(request) -> HttpResponse:
 
     context = {
         "listings": listings,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
     }
 
     return render(request, "pages/index.html", context)
