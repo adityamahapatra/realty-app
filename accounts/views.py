0519911d193b7d065e90c9632ecde48dd441a5a7
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-26 22:21:54 +0530

    Add the registration, login & dashboard templates. Set up the routing for the templates in the accounts app.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import redirect, render
+
 
 # Create your views here.
+def register(request) -> HttpResponse:
+    context = {}
+    return render(request, "accounts/register.html", context)
+
+
+def login(request) -> HttpResponse:
+    context = {}
+    return render(request, "accounts/login.html", context)
+
+
+def logout(request) -> HttpResponse:
+    return redirect("index")
+
+
+def dashboard(request) -> HttpResponse:
+    context = {}
+    return render(request, "accounts/dashboard.html", context)
