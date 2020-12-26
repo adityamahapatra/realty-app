c63ba6777c4f3e70e5d5b3128ddf8c57976d26d0
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-26 06:02:34 +0530

    Add mark up to the search template. Add filtering capabilities to the search form.

--- listings/views.py
+++ listings/views.py
@@ -3,6 +3,7 @@ from django.http.response import HttpResponse
 from django.shortcuts import get_object_or_404, render
 
 from .models import Listing
+from .choices import bedroom_choices, price_choices, state_choices
 
 
 # Create your views here.
@@ -28,4 +29,45 @@ def listing(request, listing_id: int) -> HttpResponse:
 
 
 def search(request) -> HttpResponse:
-    return render(request, "listings/search.html")
+    query_set_list = Listing.objects.order_by("-list_date")
+
+    # Keywords
+    if "keywords" in request.GET:
+        keywords = request.GET["keywords"]
+        if keywords:
+            query_set_list = query_set_list.filter(
+                description__icontains=keywords
+            )
+
+    # City
+    if "city" in request.GET:
+        city = request.GET["city"]
+        if city:
+            query_set_list = query_set_list.filter(city__iexact=city)
+
+    # State
+    if "state" in request.GET:
+        state = request.GET["state"]
+        if state:
+            query_set_list = query_set_list.filter(state__iexact=state)
+
+    # Bedrooms
+    if "bedrooms" in request.GET:
+        bedrooms = request.GET["bedrooms"]
+        if bedrooms:
+            query_set_list = query_set_list.filter(bedrooms__lte=bedrooms)
+
+    # Price
+    if "price" in request.GET:
+        price = request.GET["price"]
+        if price:
+            query_set_list = query_set_list.filter(price__lte=price)
+
+    context = {
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+        "state_choices": state_choices,
+        "listings": query_set_list,
+    }
+
+    return render(request, "listings/search.html", context)
