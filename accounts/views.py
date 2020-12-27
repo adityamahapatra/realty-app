432b28c56050fb505b86471055187963d208a37d
adityamahapatra <adi.mahapatra@gmail.com>
2020-12-27 08:29:27 +0530

    Implement new user registration.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.contrib import messages
+from django.contrib import auth, messages
+from django.contrib.auth.models import User
 from django.http.response import HttpResponse
 from django.shortcuts import redirect, render
 
@@ -6,8 +7,44 @@ from django.shortcuts import redirect, render
 # Create your views here.
 def register(request) -> HttpResponse:
     if request.method == "POST":
-        messages.error(request, "Testing error message")
-        return redirect("register")
+        # Get values from the registration form.
+        first_name = request.POST["first_name"]
+        last_name = request.POST["last_name"]
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirm_password = request.POST["confirm_password"]
+
+        # Check if the passwords match.
+        if password == confirm_password:
+            # Check if the username already exists.
+            if User.objects.filter(username=username).exists():
+                messages.error(
+                    request, f"The username '{username}' already exists."
+                )
+                return redirect("register")
+            # Check if the email is already in use.
+            elif User.objects.filter(email=email).exists():
+                messages.error(
+                    request, f"The email '{email}' is already in use."
+                )
+                return redirect("register")
+            else:
+                user = User.objects.create_user(
+                    first_name=first_name,
+                    last_name=last_name,
+                    username=username,
+                    email=email,
+                    password=password
+                )
+                # Log in the user after registration is successful.
+                user.save()
+                auth.login(request, user)
+                messages.success(request, "Registration successful!")
+                return redirect("index")
+        else:
+            messages.error(request, "The passwords do not match")
+            return redirect("register")
     else:
         context = {}
         return render(request, "accounts/register.html", context)
