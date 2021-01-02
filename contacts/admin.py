2c4d67764093c5cbd4ce446faed03167f6413e09
adityamahapatra <adi.mahapatra@gmail.com>
2021-01-03 00:22:54 +0530

    Rename the contacts model.

--- contacts/admin.py
+++ contacts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from.models import Contacts
+from .models import Contact
 
 
 # Register your models here.
@@ -10,4 +10,4 @@ class ContactAdmin(admin.ModelAdmin):
     list_per_page = 25
 
 
-admin.site.register(Contacts, ContactAdmin)
+admin.site.register(Contact, ContactAdmin)
